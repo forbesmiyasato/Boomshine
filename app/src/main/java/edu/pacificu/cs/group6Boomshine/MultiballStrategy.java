5521fa43bb7343d4ab0bf3714b70ffd151a71012
Thomas Robasciotti <47126825+roba3525@users.noreply.github.com>
2020-01-23 14:29:53 -0800

    Implemented IExplodingStrategy, MultiballStrategy, and SuperballStrategy

--- app/src/main/java/edu/pacificu/cs/group6Boomshine/MultiballStrategy.java
+++ app/src/main/java/edu/pacificu/cs/group6Boomshine/MultiballStrategy.java
@@ -1,4 +1,58 @@
 package edu.pacificu.cs.group6Boomshine;
 
+import android.content.Context;
+import android.graphics.Rect;
+import android.view.Display;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Defines the MultiballStrategy class that generates an additional
+ * initial expanding circle in a random location on the screen.
+ *
+ * This class extends the abstract IExplodingStrategy class.
+ *
+ * @author Thomas Robasciotti
+ *
+ * @version 1.0
+ */
+
 public class MultiballStrategy extends IExplodingStrategy{
+
+    /**
+     * Overrides the superclass create method.
+     *
+     * Generates an ArrayList of two expanding circles; one where the user touched
+     * on the screen and another in a random location within the screen bounds.
+     *
+     * @return An ArrayList containing two expanding circles
+     */
+
+    @Override
+    public ArrayList<ExplodingBoundedMovingCircle> create(Context context, Display display,
+                                                          int drawable, int topCoord,
+                                                          int leftCoord, int speed, int topBound,
+                                                          int bottomBound, int leftBound,
+                                                          int rightBound, int expandingSpeed,
+                                                          float radius) {
+
+        ArrayList<ExplodingBoundedMovingCircle> cRetList = new ArrayList<>();
+        Random cRandom = new Random();
+        Rect cDisplaySize = new Rect();
+
+        display.getRectSize(cDisplaySize);
+
+        int randomXCoord = cRandom.nextInt(cDisplaySize.width());
+        int randomYCoord = cRandom.nextInt(cDisplaySize.height());
+
+        cRetList.add (new ExplodingBoundedMovingCircle(context, display, drawable, topCoord,
+                leftCoord, speed, topBound, bottomBound, leftBound, rightBound, expandingSpeed,
+                radius));
+
+        cRetList.add (new ExplodingBoundedMovingCircle(context, display, drawable, randomYCoord,
+                randomXCoord, speed, topBound, bottomBound, leftBound, rightBound, expandingSpeed,
+                radius));
+
+        return cRetList;
+    }
 }
--- app/src/main/java/edu/pacificu/cs/group6Boomshine/SuperballStrategy.java
+++ app/src/main/java/edu/pacificu/cs/group6Boomshine/SuperballStrategy.java
@@ -1,4 +1,51 @@
 package edu.pacificu.cs.group6Boomshine;
 
+import android.content.Context;
+import android.graphics.Rect;
+import android.view.Display;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Defines the SuperballStrategy class that generates an initial
+ * expanding circle that will expand to twice the size of a
+ * normal expanding circle.
+ *
+ * This class extends the abstract IExplodingStrategy class.
+ *
+ * @author Thomas Robasciotti
+ *
+ * @version 1.0
+ */
+
 public class SuperballStrategy extends IExplodingStrategy {
+
+    private final int EXPANTION_MULTIPLIER = 2;
+
+    /**
+     * Overrides the superclass create method.
+     *
+     * Generates an ArrayList of one expanding circle that expands
+     * to twice the size of a normal expanding circle.
+     *
+     * @return An ArrayList containing one expanding circle
+     */
+
+    @Override
+    public ArrayList<ExplodingBoundedMovingCircle> create(Context context, Display display,
+                                                          int drawable, int topCoord,
+                                                          int leftCoord, int speed, int topBound,
+                                                          int bottomBound, int leftBound,
+                                                          int rightBound, int expandingSpeed,
+                                                          float radius) {
+
+        ArrayList<ExplodingBoundedMovingCircle> cRetList = new ArrayList<>();
+
+
+        cRetList.add (new ExplodingBoundedMovingCircle(context, display, drawable, topCoord,
+                leftCoord, speed, topBound, bottomBound, leftBound, rightBound, expandingSpeed,
+                radius * EXPANTION_MULTIPLIER));
+
+        return cRetList;
+    }
 }
