2cad12c722911789c3c1cefa36b961efe598e430
forbesmiyasato <miya5288@pacificu.edu>
2020-01-26 01:55:49 -0800

    finished high scores view

--- app/src/main/java/edu/pacificu/cs/group6Boomshine/HighScoreView.java
+++ app/src/main/java/edu/pacificu/cs/group6Boomshine/HighScoreView.java
@@ -1,23 +1,15 @@
 package edu.pacificu.cs.group6Boomshine;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
-import android.media.MediaPlayer;
 import android.util.Log;
-import android.view.Display;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Random;
 
 import edu.pacificu.cs.group6Boomshine.edu.pacificu.cs.httprequests.HttpService;
 import edu.pacificu.cs.group6Boomshine.edu.pacificu.cs.httprequests.RetrofitClient;
@@ -36,7 +28,12 @@ public class HighScoreView extends ImageView
   private JSONArray mHighScores;
   private CompositeDisposable mcCompositeDisposable;
   private HttpService mService;
-
+  //View placement variables
+  private final double X_SCALE_FACTOR = 4; //To make text x position responsive
+  private final double Y_SCALE_FACTOR = 4; //To make text y position responsive
+  private final int BACK_BUTTON_WIDTH = 400;
+  private final int BACK_BUTTON_HEIGHT = 200;
+  private final int Y_INCREMENT = 100;
   public HighScoreView(Context context)
   {
     super(context);
@@ -64,38 +61,56 @@ public class HighScoreView extends ImageView
   @Override
   public void onDraw(Canvas canvas)
   {
-    final double SCALE_FACTOR = 4;
     mWidth = getWidth();
     mHeight = getHeight();
     String HighScore;
     String Name;
+    mPaint.setTextSize(mHeight * 0.05f);
+    mPaint.setColor(getResources().getColor(R.color.cBlack));
+    mPaint.setStyle(Paint.Style.FILL);
+    mPaint.setTextAlign(Paint.Align.CENTER);
+
+    canvas.drawText("High Scores",
+            (int) (mWidth / 2), (int)(mHeight / (Y_SCALE_FACTOR * 2)), mPaint);
+
     for (int i = 1; i <= 5; i++) {
       try {
 
         HighScore = mHighScores.getJSONObject(i - 1).getString("HighScore");
         Name = mHighScores.getJSONObject(i - 1).getString("Name");
-        mPaint.setTextSize(mHeight * 0.05f);
-        mPaint.setColor(getResources().getColor(R.color.cBlack));
-        mPaint.setStyle(Paint.Style.FILL);
-        mPaint.setTextAlign(Paint.Align.CENTER);
 
-        Log.d("ONDRAW", String.valueOf(mWidth));
         canvas.drawText(HighScore,
-                (int) (mWidth / SCALE_FACTOR), 100 * i, mPaint);
-        canvas.drawText(Name, (int) (mWidth * 3 / SCALE_FACTOR), 100 * i, mPaint);
+                (int) (mWidth / X_SCALE_FACTOR), Y_INCREMENT * i
+                        + (int) (mHeight / Y_SCALE_FACTOR), mPaint);
+        canvas.drawText(Name, (int) (mWidth * 3 / X_SCALE_FACTOR),
+                Y_INCREMENT * i + (int) (mHeight / Y_SCALE_FACTOR), mPaint);
       } catch (JSONException e) {
         e.printStackTrace();
       }
     }
+    Drawable back = getResources().getDrawable(R.drawable.backtomenu, null);
+    back.setBounds((mWidth - BACK_BUTTON_WIDTH) / 2, Y_INCREMENT * 5 + (int) (mHeight / Y_SCALE_FACTOR),
+            (mWidth - BACK_BUTTON_WIDTH) / 2 + BACK_BUTTON_WIDTH,
+            Y_INCREMENT * 5 + (int) (mHeight / Y_SCALE_FACTOR) + BACK_BUTTON_HEIGHT);
+    back.draw(canvas);
   }
 
   @Override
   public boolean onTouchEvent(MotionEvent event)
   {
+    int x = (int) event.getX();
+    int y = (int) event.getY();
     if (event.getAction() != MotionEvent.ACTION_DOWN) {
       return super.onTouchEvent(event);
     }
 
+    if ((x >= (mWidth - BACK_BUTTON_WIDTH) / 2
+            && x <= (mWidth - BACK_BUTTON_WIDTH) / 2 + BACK_BUTTON_WIDTH)
+    && (y >= Y_INCREMENT * 5 + (int) (mHeight / Y_SCALE_FACTOR)
+            && y <= Y_INCREMENT * 5 + (int) (mHeight / Y_SCALE_FACTOR) + BACK_BUTTON_HEIGHT))
+    {
+      mcGame.onBackClicked();
+    }
 
     return true;
   }
